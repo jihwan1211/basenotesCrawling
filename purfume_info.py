b343ebef234fd1c0eeb89518684be5f8075e320c
jihwan1211 <jhwank1211@gmail.com>
2023-09-20 17:45:49 +0900

    second commit

--- purfume_info.py
+++ purfume_info.py
@@ -1,73 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 
-# # .txt 파일 경로 지정
-# file_path = 'perfume_url.txt'
+# .txt 파일 경로 지정
+file_path = 'perfume_url.txt'
 
-# # URL을 저장할 리스트 생성
-# urls = []
+# URL을 저장할 리스트 생성
+urls = []
 
-# # .txt 파일에서 URL 읽어오기
-# with open(file_path, 'r', encoding='utf-8') as file:
-#     for line in file:
-#         # 각 줄에서 줄바꿈 문자 및 공백 제거하여 URL 추출
-#         url = line.strip()
-#         if url:
-#             urls.append(url)
+# .txt 파일에서 URL 읽어오기
+with open(file_path, 'r', encoding='utf-8') as file:
+    for line in file:
+        # 각 줄에서 줄바꿈 문자 및 공백 제거하여 URL 추출
+        url = line.strip()
+        if url:
+            urls.append(url)
 
-# # URLs 리스트 확인
+# URLs 리스트 확인
 # print(urls)
 
-response = requests.get('https://basenotes.com/fragrances/no-5-by-chanel.10210628', headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'})
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-# spans = soup.select('span[class=h1_fragname]')
-# 향수 이름
-# name_span = soup.find('span', class_='h1_fragname')
-# name = name_span.text
+# user-agent는 각자 컴퓨터에 맞는 걸로
+purfume_id = 0
+for url in urls:
+    try:
+        response = requests.get(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'})
+        if response.status_code == 200:
+   
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+    # spans = soup.select('span[class=h1_fragname]')
+            # 향수 이름
+            name_span = soup.find('span', class_='h1_fragname')
+            name = name_span.text
 
-# 향수 이미지
-# img_div= soup.find('div', class_='product_image')
-# img = img_div.find('img')
-# img_src = img.get('src')
+    # 향수 이미지
+            img_div= soup.find('div', class_='product_image')
+            img = img_div.find('img')
+            img_src = img.get('src')
 
-# 탑 노트
-# ul = soup.find('ul', class_='fragrancenotes')
-# uls = ul.select("ul")
-# # print(lis)
+    # 노트들
+            ul = soup.find('ul', class_='fragrancenotes')
+            uls = ul.select("ul")
 
-# for ul in uls:
-#     a=ul.find('li')
-#     words = a.text.split(',')
-#     print(words)
+            for i in range(0, len(uls)):
+                note = uls[i].find('li')
+                words = note.text.split(',')
+                if(i==0): 
+                    top_notes = words
+                elif(i==1): 
+                    middle_notes = words
+                else: 
+                    base_notes = words
+    
+            data= {"id": purfume_id,
+                   "image_src": img_src, 
+                   "name": name, 
+                   "top_notes": top_notes, 
+                   "middle_notes": middle_notes, 
+                   "base_notes": base_notes}
+            purfume_id += 1
+            print(data)
 
-
-
-
-
-
-# URL 리스트를 순회하며 크롤링
-# for url in urls:
-#     try:
-#         response = requests.get(url)
-#         if response.status_code == 200:
-#             # 웹 페이지의 HTML 내용을 추출
-#             html = response.text
-
-#             # BeautifulSoup를 사용하여 HTML 파싱 및 원하는 데이터 추출
-#             soup = BeautifulSoup(html, 'html.parser')
-            
-#             # 원하는 작업 수행 (예: 데이터 추출 또는 가공)
-
-#             # 결과 출력 또는 다른 작업 수행
-#             print(f'크롤링 성공: {url}')
-#         else:
-#             print(f'크롤링 실패: {url}, 응답 코드: {response.status_code}')
-#     except Exception as e:
-#         print(f'오류 발생: {url}, 오류 내용: {str(e)}')
+    except Exception as e:
+        print(f'오류 발생: {url}, 오류 내용: {str(e)}')
 
 
-# file_path = 'perfume_url.txt'
+
+# file_path = 'perfume_info.txt'
 
 # with open(file_path, 'a', encoding='utf-8') as file:
 #     for result in results:
